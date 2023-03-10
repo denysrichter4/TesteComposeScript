4e47e714466da64766f70631c07fd495a50970f2
denysrichter4 <denysrichter4@gmail.com>
2023-03-09 21:06:24 -0300

    initial commmit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            findProject(file("gradle/libs.versions.toml"))
+        }
+    }
+}
+rootProject.name = "TesteComposeLibsScript"
+include(":app")
